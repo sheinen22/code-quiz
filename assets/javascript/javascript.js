eadefded93f8f1349ef03b3d301e3f0645555e59
Stephen Heinen <stephenheinen22@yahoo.com>
2022-06-28 00:13:40 -0500

    added quiz questions and answers, added event listener for start quiz button, next up: first function

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,45 @@
+
+var qAndAs = [
+    {
+        question: "What does HTML stand for?",
+        optionA: "Hyper Text Markup Language",
+        optionB: "Hyper Text Multiple Language",
+        optionC: "Hyper Tools Multi Language",
+        optionD: "Hyper Trigger Mini Language",
+        Correct: "What does HTML stand for?"
+    }, {
+        question: "What does CSS stand for?",
+        optionA: "Colorful Style Sheet",
+        optionB: "Centralized Style Sheet",
+        optionC: "Coordinated Style Sheet",
+        optionD: "Cascading Style Sheet",
+        Correct: "Cascading Style Sheet"
+    }, {
+        question: "What character represents 'class' in CSS?",
+        optionA: "#",
+        optionB: ":",
+        optionC: ".",
+        optionD: "-",
+        correct: "."
+    }, {
+        question: "What flag adds a message in Git?",
+        optionA: "/m",
+        optionB: "[m",
+        optionC: "-m",
+        optionD: ".m",
+        correct: "-m"
+    }, {
+        question: "In CSS, what character allows you to influence all aspects?",
+        optionA: ".",
+        optionB: "*",
+        optionC: "#",
+        optionD: "@",
+        correct: "*"
+    }
+];
+
+start-btn.addEventListener("click", startQuiz);
+
+function startQuiz () {
+    
+}
